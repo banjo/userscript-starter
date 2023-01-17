c936d555213454f29df1afb8445e37c156fb62c2
Anton Ödman <anton.odman@gmail.com>
2023-01-17 13:25:21 +0100

    chore: initial setup

---
+++ dist/userscript-starter.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       userscript-starter
+// @namespace  banjoanton
+// @version    0.0.0
+// @author     monkey
+// @icon       https://vitejs.dev/logo.svg
+// @match      https://www.google.com/
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    console.log("hello world");
+})();
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import monkey from "vite-plugin-monkey";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        monkey({
+            entry: "src/main.ts",
+            userscript: {
+                icon: "https://vitejs.dev/logo.svg",
+                namespace: "banjoanton",
+                author: "banjoanton",
+                match: ["https://www.google.com/"],
+                "run-at": "document-idle",
+            },
+        }),
+    ],
+});
